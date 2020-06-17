6e273ee6592dee6d0bf8f56ff3f64f0a3b0ff856
nicoWeb31 <nico.riot@free.fr>
2020-06-17 12:10:49 +0200

    add component

---
+++ src/app/inbox/email-replay/email-replay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-replay',
+  templateUrl: './email-replay.component.html',
+  styleUrls: ['./email-replay.component.css']
+})
+export class EmailReplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
+import { EmailCreateComponent } from './email-create/email-create.component';
+import { EmailReplayComponent } from './email-replay/email-replay.component';
+import { EmailIndexComponent } from './email-index/email-index.component';
+import { EmailShowComponent } from './email-show/email-show.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, EmailCreateComponent, EmailReplayComponent, EmailIndexComponent, EmailShowComponent],
   imports: [
     CommonModule,
     InboxRoutingModule
