c2dfeed716c86a274b44fe4f976be170b626a66f
nicoWeb31 <nico.riot@free.fr>
2020-06-17 14:52:52 +0200

    html

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+
+
+
+interface EmailSumary {
+  id: string,
+  subject: string,
+  from: string  
+
+
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
 
-  constructor() { }
+
+  private rootUrl = 'https://api.angular-email.com';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  //GET EMAIL
+  getEmails(){
+    return this.http.get<EmailSumary[]>(`${this.rootUrl}/emails`)
+  }
+
+
+
+
+
 }
