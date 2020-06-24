0eec63dc05eae3a12c393c5d9a23aceae8659087
nicoWeb31 <nico.riot@free.fr>
2020-06-24 14:58:23 +0200

    end ok

--- src/app/inbox/email-replay/email-replay.component.ts
+++ src/app/inbox/email-replay/email-replay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import { Email } from '../email';
 import { EmailService } from '../email.service';
 
@@ -7,9 +7,9 @@ import { EmailService } from '../email.service';
   templateUrl: './email-replay.component.html',
   styleUrls: ['./email-replay.component.css']
 })
-export class EmailReplayComponent implements OnInit {
+export class EmailReplayComponent {
 
-  email: Email
+  @Input() email: Email
   showModal = false
 
 
@@ -17,10 +17,28 @@ export class EmailReplayComponent implements OnInit {
     private emailServ : EmailService
   ) { }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    const text = this.email.text.replace(/\n/gi, '\n> ')
+    
+    
+    this.email = {
+      /// on inverce les valeurs pour le reply
+      ...this.email,
+      from : this.email.to,
+      to : this.email.from,
+      subject: `RE : ${this.email.subject}`,
+      text:`\n\n\n ----------- ${this.email.from} wrote:\n> ${text}`
+
+    }
   }
 
   onSubmit(email: Email){
+
+    this.emailServ.sendEmail(email).subscribe(()=>{
+
+      this.showModal = false
+
+    })
 
   }
 
